740c45f724903a2d4f430eeba6d1fef3a07520ce
ToOro <ToOro@localhost>
2017-04-05 16:13:40 -0500

    Creating

---
+++ webscrape_java/src/main/java/com/tooro/webscrape_java/App.java
@@ -0,0 +1,13 @@
+package com.tooro.webscrape_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
